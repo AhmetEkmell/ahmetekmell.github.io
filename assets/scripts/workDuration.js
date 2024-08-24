9fd0a41b6e1b6b93db358f1cd23d521db2e355a3
Ahmet Ekmel YEŞİLYURT <ekmel.147@gmail.com>
2024-08-25 02:11:58 +0300

    Update workDuration.js

--- assets/scripts/workDuration.js
+++ assets/scripts/workDuration.js
@@ -3,13 +3,9 @@ const workDuration = document.getElementById("work-duration");
 const startDate = new Date("2022-03-01");
 const currentDate = new Date();
 
-let yearDiff = currentDate.getFullYear() - startDate.getFullYear();
-let monthDiff = currentDate.getMonth() - startDate.getMonth();
+const totalMonths = (currentDate.getFullYear() - startDate.getFullYear()) * 12 + (currentDate.getMonth() - startDate.getMonth());
+const years = Math.floor(totalMonths / 12);
+const months = totalMonths % 12;
 
-if (monthDiff < 0) {
-  yearDiff--;
-  monthDiff += 12;
-}
-
-const dynamicDuration = yearDiff + " year and " + monthDiff + " months.";
+const dynamicDuration = (years + months / 12).toFixed(1);
 workDuration.textContent = dynamicDuration;
